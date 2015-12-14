cca1d8f99f2ca9219a13f75015278e3404680494
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-14 10:34:10 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
+var _ = require('lodash'),
+    util = require('./util.js');
+
+var request = require('request').defaults({
+    baseUrl: 'https://api.bitbucket.org/2.0/'
+});
+
+
+var globalPickResult = {
+    'user_uuid': 'user.uuid',
+    'role': 'role',
+    'user_username': 'user.username',
+    'user_display_name': 'user.display_name',
+    'user_url': 'user.links.self.href',
+    'approved': 'isApproved'
+};
+
 module.exports = {
+
+    authParams: function (dexter) {
+        var auth = {},
+            username = dexter.environment('bitbucket_username'),
+            password = dexter.environment('bitbucket_password');
+
+        if (username && password) {
+
+            auth.user = username;
+            auth.pass = password;
+        }
+
+        return _.isEmpty(auth)? false : auth;
+    },
+
+    processResult: function (error, responce, body) {
+
+        if (error)
+
+            this.fail(error);
+
+        else if (responce && !body)
+
+            this.fail(responce.statusCode + ': Something is happened');
+
+        else if (responce && body.error)
+
+            this.fail(responce.statusCode + ': ' + JSON.stringify(body.error));
+
+        else
+
+            this.complete(util.pickResult(body, globalPickResult));
+
+    },
+
+    checkCorrectParams: function (auth, step) {
+        var result = true;
+
+        if (!auth) {
+
+            result = false;
+            this.fail('A [bitbucket_username, bitbucket_password] environment need for this module.');
+        }
+
+        if (!step.input('owner').first() || !step.input('repo_slug').first()) {
+
+            result = false;
+            this.fail('A [owner, repo_slug, revision] inputs need for this module.');
+        }
+
+        return result;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +76,20 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var auth = this.authParams(dexter),
+            requestId = step.input('id').first(),
+            owner = step.input('owner').first(),
+            repo_slug = step.input('repo_slug').first();
+
+        var uriLink = 'repositories/' + owner + '/' + repo_slug + '/pullrequests/' + requestId + '/approve';
+
+        // check params.
+        if (!this.checkCorrectParams(auth, step)) return;
+        //send API request
+        request.post({url: uriLink, auth: auth, json: true}, function (error, responce, body) {
+            console.log(body);
+            this.processResult(error, responce, body);
+        }.bind(this));
     }
 };
